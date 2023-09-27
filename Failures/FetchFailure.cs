d27116bf9830eb46ece904d3f2f70e292003aabb
Vít Němec <vitek2004@seznam.cz>
2023-09-27 09:22:41 +0200

    init

---
+++ Failures/FetchFailure.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WindowsFormsApp2.Failures.FetchFailure
+// Assembly: WindowsFormsApp2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 6013BA42-02F4-4668-A0E9-E34B7235D0BC
+// Assembly location: C:\Users\bojac\Downloads\WindowsFormsApp2.exe
+
+namespace WindowsFormsApp2.Failures
+{
+  public class FetchFailure : NetworkFailure
+  {
+    public FetchFailure(string message)
+      : base(message)
+    {
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: WindowsFormsApp2.Program
+// Assembly: WindowsFormsApp2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 6013BA42-02F4-4668-A0E9-E34B7235D0BC
+// Assembly location: C:\Users\bojac\Downloads\WindowsFormsApp2.exe
+
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+  internal static class Program
+  {
+    [STAThread]
+    private static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run((Form) new Form1());
+    }
+  }
+}
